1710229393c59b3008403818bddb8766243af867
BHess2653 <GM_Dahaka@BHMac.local>
2017-09-20 10:16:02 -0400

    editing config.js

--- teamapi.config.js
+++ teamapi.config.js
@@ -1,16 +1,16 @@
-.exports = {
-  :[
-    name: 'bhessFireHouseSubs',
-    scripts: 'src/server.js',
+module.exports = {
+  apps:[
+    name      : 'teamapi',
+    script    : 'src/server.js',
     env: {
       DB_HOST: 'localhost',
       DB_PASS: '',
       DB_USER: 'root',
-      DB_NAME: 'teamapi',
+      DB_NAME: 'teamApi',
       DB_SCHEMA: 'mysql',
       DB_PORT: 3306
     },
-    env_production: {
+    env_production : {
       NODE_ENV: 'production'
     }
   ]
